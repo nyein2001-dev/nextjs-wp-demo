41169ab0c0c197beb84d98f19bb855c4d2944f35
nyein2001-dev <nyeinchantoe0@gmail.com>
2024-11-17 17:52:11 +0630

    update layout widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
 import { Button } from "@/components/ui/button";
 import { Main } from "@/components/craft";
-import { mainMenu } from "@/menu.config";
+import { mainMenu, contentMenu } from "@/menu.config";
+import { Section, Container } from "@/components/craft";
+import Balancer from "react-wrap-balancer";
 
 import Logo from "@/public/next.svg";
 
@@ -49,7 +52,9 @@ export default function RootLayout({
         >
           <Nav />
           <Main>{children}</Main>
+          <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
@@ -100,5 +105,61 @@ const Nav = ({ className, children, id }: NavProps) => {
         </div>
       </div>
     </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <Section>
+        <Container className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] gap-12">
+          <div className="flex flex-col gap-6 not-prose">
+            <Link href="/">
+              <h3 className="sr-only">brijr/components</h3>
+              <Image
+                src={Logo}
+                alt="Logo"
+                width={120}
+                height={27.27}
+                className="dark:invert hover:opacity-75 transition-all"
+              ></Image>
+            </Link>
+            <p>
+              <Balancer>{metadata.description}</Balancer>
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 text-sm">
+            <h5 className="font-medium text-base">Website</h5>
+            {Object.entries(mainMenu).map(([key, href]) => (
+              <Link
+                className="hover:underline underline-offset-4"
+                key={href}
+                href={href}
+              >
+                {key.charAt(0).toUpperCase() + key.slice(1)}
+              </Link>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2 text-sm">
+            <h5 className="font-medium text-base">Blog</h5>
+            {Object.entries(contentMenu).map(([key, href]) => (
+              <Link
+                className="hover:underline underline-offset-4"
+                key={href}
+                href={href}
+              >
+                {key.charAt(0).toUpperCase() + key.slice(1)}
+              </Link>
+            ))}
+          </div>
+        </Container>
+        <Container className="border-t not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center">
+          <p className="text-muted-foreground">
+            © <a href="https://9d8.dev">9d8</a>. All rights reserved.
+            2024-present.
+          </p>
+        </Container>
+      </Section>
+    </footer>
   );
 };
